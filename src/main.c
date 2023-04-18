b8804bf042beb9a1bb2e328aacadfcc44b6f1938
MateoG404 <mgutierrezca@unal.edu.co>
2023-04-18 07:00:58 -0500

    Este

--- src/main.c
+++ src/main.c
@@ -1,54 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
 #include <sys/types.h>
+#include <sys/shm.h>
 #include "interfaz.h"
 #include "search.h"
 
-int main(){
-    
-    int* data;
-    /*Crear una tubería que se encargue de la comunicación entre la interfaz y la busqueda o back*/
-    int pipefd[2];
-    int r=pipe(pipefd);
-    if(r==-1){
-        perror("Error en la creación de tuberías");
-        exit(EXIT_FAILURE);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+int* input();
+main(){
+    // Variables
 
+    key_t key= 456;//llave para la memoria compartida
+    int *shm;//puntero a la memoria compartida
+    int shmId,r;//identificador de la memoria compartida
+    
+    //crear espacio de memoria compartida
+    
+    shmId = shmget(key, 3*sizeof(float), 0666|IPC_CREAT);
+    
+    if(shmId < 0){ // Error en la creación de memoria compartida
+        perror("Error en shmget");
+        exit(EXIT_FAILURE);
     }
     
+    //Asociar espacio de memoria creada a este proceso
+
+    shm = shmat(shmId, NULL, 0);
+
     /*Crear dos procesos*/
     __pid_t pid=fork();
-
+    
     if(pid==-1){
         perror("Error en la creación de procesos");
         exit(EXIT_FAILURE);
-    }   
+    }
     else if(pid==0){
-        /*Hijo*/
-        int* aux;
-        aux=home();
-        /*Escritura de datos con pipe*/
-        close(pipefd[0]);
-        r=write(pipefd[1],aux,sizeof(int*));
-        if(r==-1){
-            perror("Error en la escritura de datos con pipe");
-        }
-        close(pipefd[1]);
-        exit(EXIT_SUCCESS);
+        // Se ejecuta la interfaz del programa mandando el puntero de la memoria compartida
+        input(shm);
+
     }else{
         /*Padre*/
+        
         while (wait(NULL)!=-1){/*Esperar hasta que el proceso hijo termine su ejecución*/
-            /*Lectura de datos con pipe*/
-            close(pipefd[1]);
-            r=read(pipefd[  0],data,sizeof(int*));
-            if(r==-1){
-                perror("Error en la lectura de datos con pipe");
-            }
-            close(pipefd[0]);
-            prueba(data);
+
+            // Se ejecuta la busqueda haciendo uso de la memoria compartida con los datos expuestos    
+            search(shm);
+            
         }
     }
+    r = shmdt(shm);  //desasociar espacio de memoria compartida
+
     return 0;
 }
